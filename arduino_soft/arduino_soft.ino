f3d80e3710781d9309730cbd71604a66072c0369
Ernestas <e.konopliov@gmail.com>
2019-05-15 10:01:19 +0300

    final commit

--- arduino_soft/arduino_soft.ino
+++ arduino_soft/arduino_soft.ino
@@ -4,6 +4,7 @@
 // Connecting to the internet
 char * ssid = "iPhone X";
 char * password = "12345678";
+int notificationPin = 7;
 
 // Controlling the WS2812B
 
@@ -17,58 +18,66 @@ void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+  Serial.println("");
   while(WiFi.status()!=WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
   Serial.println("");
+  Serial.println("Connected to WiFi.");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
-
-  // Initializing the websocket communication
+  
 // webSocket.begin("172.20.10.12", 8000, "/subscribe");
 
   webSocket.begin("attentionnotifier999.herokuapp.com", 80, "/subscribe");
   webSocket.onEvent(webSocketEvent);
-  webSocket.setReconnectInterval(50); 
+  webSocket.setReconnectInterval(10); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   webSocket.loop();
-  
-
 } 
 
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
   switch(type) {
     case WStype_DISCONNECTED:
-      Serial.printf("[WSc] Disconnected!\n");
+      Serial.printf("Socket disconnected!\n");
       break;
     case WStype_CONNECTED:
-      Serial.printf("[WSc] Connected to url: %s\n", payload);
+      Serial.printf("Socket connected to url: %s\n", payload);
       break;
     case WStype_TEXT:
       if((char)payload[0] == '1'){
-      Serial.printf("Received button push!");
+      Serial.println("Received button push!");
       webSocket.sendTXT("done");
-      blinkTheLed();
+      blinkTheLed(3);
+      notify();
       }
-      if((char)payload[0] == '3'){
-      Serial.printf("Received connection!");
+      else if((char)payload[0] == '3'){
+      Serial.println("New connection!");
       webSocket.sendTXT("connected");
-      blinkTheLed();
+      blinkTheLed(6);
       }
             
       break;
   }
 }
 
-void blinkTheLed(){
-   for(int i = 0; i < 6; i++){
+void blinkTheLed(int howManyTimes){
+   for(int i = 0; i < howManyTimes; i++){
      digitalWrite(BUILTIN_LED, HIGH);
-     delay(300);
+     delay(150);
      digitalWrite(BUILTIN_LED, LOW);
-     delay(300);
+     delay(150);
     }
+  }
+
+void notify(){
+     for(int i = 0; i < 5; i++){
+       digitalWrite(notificationPin, HIGH);
+       delay(250);
+       digitalWrite(notificationPin, LOW);
+       delay(250);
+      }
   }
